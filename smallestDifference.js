73ab528f8bf27c3bdb4caae51fd0bf7e42a61afc
eightseconds <paulseokcho@gmail.com>
2019-10-08 23:11:34 -0700

    Optimize time complexity

--- smallestDifference.js
+++ smallestDifference.js
@@ -1,8 +1,30 @@
 function smallestDifference(arrayOne, arrayTwo) {
-    arrayOne.sort((a,b) => a - b)
-    arrayTwo.sort((a,b) => a - b)
+    arrayOne.sort((a, b) => a - b)
+    arrayTwo.sort((a, b) => a - b)
 
     let idxOne = 0
     let idxTwo = 0
+    let smallest = Infinity
+    let difference = Infinity
+    let smallestPair = [];
+
+    while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+        let firstNum = arrayOne[idxOne];
+        let secondNum = arrayTwo[idxTwo];
+        if (firstNum < secondNum) {
+            difference = secondNum - firstNum
+            idxOne++
+        } else if (firstNum > secondNum) {
+            difference = firstNum - secondNum
+            idxTwo++
+        } else {
+            return [firstNum, secondNum]
+        }
+        if (smallest > difference) {
+            smallest = difference;
+            smallestPair = [firstNum, secondNum]
+        }
+    }
+    return smallestPair
 }
 
